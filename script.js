fdd1275a7c4e02febfb48ede692871fb6c151ae9
Pages of Pombagiras <almasdepombagiras@gmail.com>
2026-09-26 11:45:30 -0300

    feat(figueira): aplicar padrao mestre editorial, SEO, dados estruturados, sitemap, busca e carrossel

--- script.js
+++ script.js
@@ -364,7 +364,7 @@ const carouselImages = [
     { name: "Da Praia", url: "https://cdn.pombagiras.com/fotos/pombagira_da_praia.png" },
     { name: "Maria Farrapo", url: "https://cdn.pombagiras.com/fotos/pombagira_maria_farrapo.png", link: "maria-farrapo/index.html" },
     { name: "Menina", url: "https://cdn.pombagiras.com/fotos/pombagira_menina.png" },
-    { name: "Da Figueira", url: "https://cdn.pombagiras.com/fotos/pombagira_da_figueira.png" },
+    { name: "Da Figueira", url: "https://cdn.pombagiras.com/fotos/pombagira_da_figueira.png", link: "figueira/index.html" },
     { name: "Das Almas", url: "https://cdn.pombagiras.com/fotos/pombagira_das_almas.png" },
     { name: "Sete Encruzilhadas", url: "https://cdn.pombagiras.com/fotos/pombagira_sete_encruzilhadas.png" },
     { name: "Da Lua", url: "https://cdn.pombagiras.com/fotos/pombagira_da_lua.png" },
